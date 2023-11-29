758365cf3a0541218d9f2a67a1dff99109808ae7
Rajeshaachari <107452668+RajeshAachari@users.noreply.github.com>
2023-11-29 22:57:24 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""rajesh's command-line utility for administrative tasks."""
 import os
 import sys
 
@@ -8,7 +8,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notesapp.settings')
     try:
-        from django.core.management import execute_from_command_line
+        from rajesh.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
